4ffa4cb141a5d2dacfd7effd63b6dce05f889aed
Fikdk <luigi.dv@hotmail.com>
2023-04-06 17:08:31 -0300

    trabalho feito!!

--- javascript/audio.js
+++ javascript/audio.js
@@ -2,3 +2,9 @@
     var audio = document.getElementById("bolha");
     audio.play();
   }
+
+  window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
